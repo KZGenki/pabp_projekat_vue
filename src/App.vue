bc9234b3a910e7d7c9da5c48b5ab0ee94ffc1a0a
Ognjen Baltic RIN-3/22 <ognjenrin322@gs.viser.edu.rs>
2023-12-10 14:40:24 +0100

    Snimanje izmena podataka o studentu

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { computed, onMounted, reactive, ref } from 'vue';
 import TabelaStudenata from './components/TabelaStudenata.vue';
 import Pretraga from './components/Pretraga.vue';
+import StudentForma from './components/StudentForma.vue';
 import axios from 'axios'
 
 const Studenti = ref([])
 const kriterijum = ref("")
+const student = ref()
 const DohvatiStudente = () => {
   axios.get("http://94.156.189.137:8000/api/Employees")
     .then((response) => {
@@ -16,10 +18,23 @@ const DohvatiStudente = () => {
       console.log("Error ", err);
     })
 }
+const IzmeniStudenta = (arg)=>{
+  console.log(arg, student.value);
+  if(student.value){
+    axios.put(`http://94.156.189.137:8000/api/Employees/${student.value.employeeId}`,{'employeeId':student.value.employeeId, 'firstName':arg.firstName, 'lastName':arg.lastName})
+  .then((response)=>{
+    console.log(response);
+    student.value = null
+    DohvatiStudente()
+  })
+  .catch((err)=>console.log(err))
+  }
+}
 onMounted(() => {
   DohvatiStudente()
 })
 const Test = (arg) => {
+  student.value = arg
   console.log(arg);
 }
 
@@ -29,9 +44,11 @@ const filtrirano = computed(() => {
   })
 })
 
+
 </script>
 
 <template>
+  <StudentForma :student="student" @sacuvaj="IzmeniStudenta"></StudentForma>
   <Pretraga @pretraga="arg => kriterijum = arg"></Pretraga>
   <TabelaStudenata :studenti="filtrirano" @izmeni="Test"></TabelaStudenata>
 </template>
---
+++ src/components/StudentForma.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onUpdated, ref, watch } from 'vue';
+const props = defineProps(["student"])
+const emits = defineEmits(["sacuvaj"])
+const data = ref()
+const ime = ref("")
+const prezime = ref("")
+watch(data,()=>{
+    if(data.value){
+        ime.value = data.value.firstName
+        prezime.value = data.value.lastName
+    }
+    else{
+        ime.value=""
+        prezime.value=""
+    }
+})
+onUpdated(()=>{
+    data.value = props.student
+
+})
+</script>
+<template>
+    <table>
+        <tr>
+            <td>Ime: </td>
+            <td><input type="text" v-model="ime"></td>
+        </tr>
+        <tr>
+            <td>Prezime: </td>
+            <td><input type="text" v-model="prezime"></td>
+        </tr>
+        <tr><td colspan="2"><button style="width:100%" @click="$emit('sacuvaj',{firstName:ime,lastName:prezime})">Sacuvaj</button></td></tr>
+    </table>
+</template>
+<style scoped></style>
